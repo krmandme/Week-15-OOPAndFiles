f965f58ec89478cf78d8cec21a89c169bb9df1d7
Kristjan Mändmets <kristjan.mandmets@ppe.ee>
2022-06-03 16:00:16 +0300

    Viimane projekt lisatud, jee!

---
+++ Frozen/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Frozen
+{
+    class Program
+    {
+        class SecretSanta
+        {
+            string name;
+            string gift;
+
+
+            public SecretSanta(string _name, string _gift)
+            {
+                name = _name;
+                gift = _gift;
+            }
+
+
+            public string Name
+            {
+                get { return name; }
+            }
+
+
+            public string Gift
+            {
+                get { return gift; }
+            }
+
+
+        }
+        static void Main(string[] args)
+        {
+            List<SecretSanta> frozenGift = new List<SecretSanta>();
+            string[] giftsFromFile = GetDataFromFile();
+
+            foreach (string line in giftsFromFile)
+            {
+                string[] tempArray = line.Split(new char[] { '/' }, StringSplitOptions.RemoveEmptyEntries);
+                SecretSanta newGift = new SecretSanta(tempArray[0], tempArray[1]);
+                frozenGift.Add(newGift);
+            }
+
+            foreach (SecretSanta giftFromList in frozenGift)
+            {
+                Console.WriteLine($"{giftFromList.Name} wants {giftFromList.Gift} for Christmas.");
+            }
+        }
+
+        public static void DisplayElementsFromArray(string[] someArray)
+        {
+            foreach (string element in someArray)
+            {
+                Console.WriteLine($"String from array: {element}");
+            }
+        }
+
+
+        public static string[] GetDataFromFile()
+        {
+            string filePath = @"C:\Users\Kris...\samples\frozen.txt";
+            string[] dataFromFile = File.ReadAllLines(filePath);
+
+            return dataFromFile;
+        }
+    }
+}
